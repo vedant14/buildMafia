0d671e257199466da60da02ea2a04b7651512e06
Vedant Lohbare <vedant.lohbare@gmail.com>
2021-11-11 10:40:32 +0530

    Test with hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -28,33 +28,32 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
-  // render() {
-  //   return (
-  //     <Html>
-  //       <Head>
-  //         {/*   {/* Global Site Tag (gtag.js) - Google Analytics */} */}
-  //         {/*   <script */}
-  //         {/*     async */}
-  //         {/*     src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} */}
-  //         {/*   /> */}
-  //         {/*   <script */}
-  //         {/*     dangerouslySetInnerHTML={{ */}
-  //         {/*       __html: ` */}
-  //         {/*     window.dataLayer = window.dataLayer || []; */}
-  //         {/*     function gtag(){dataLayer.push(arguments);} */}
-  //         {/*     gtag('js', new Date()); */}
-  //         {/*     gtag('config', '${GA_TRACKING_ID}', { */}
-  //         {/*       page_path: window.location.pathname, */}
-  //         {/*     }); */}
-  //         {/*   `, */}
-  //         {/*     }} */}
-  //         {/*   /> */}
-  //       </Head>
-  //       <body>
-  //         <Main />
-  //         <NextScript />
-  //       </body>
-  //     </Html>
-  //   );
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2676713,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              });
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
